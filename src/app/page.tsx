6b0298324482c9de8b985f07a1c969a633771233
luizfranco23 <luizfranco@wine.com.br>
2025-03-20 20:16:32 -0300

    feat: added project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,67 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { useEffect, useState } from "react";
+import Image from 'next/image';
 
-export default function Home() {
+export default function Vix() {
+  const apiUrl = process.env.NEXT_PUBLIC_API_JSON;
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (!apiUrl) {
+          throw new Error("API URL não está definido");
+        }
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+          throw new Error("Falha ao buscar os dados");
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (e) {
+        console.error(e);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [apiUrl]);
+
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  const colorPrimary = data?.designSystem?.Primary;
+  const colorSecondary = data?.designSystem?.Secondary;
+  const image = data?.image?.formats?.thumbnail?.url;
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+    <div>
+      <h1>Prefeitura de: {data?.name}</h1>
+      <h2>Descrição: {data?.identity}</h2>
+
+      <div style={{ marginTop: 20, marginBottom: 20 }} />
+      <div style={{ width: 200, height: 200, background: colorPrimary }}>
+        Teste cor primária
+      </div>
+
+      <div style={{ width: 200, height: 200, background: colorSecondary }}>
+        Teste cor secundária
+      </div>
+
+      {image && (
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          height={236}
+          width={236}
+          src={image}
+          alt="Imagem Thumbnail"
           priority
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      )}
     </div>
   );
 }
